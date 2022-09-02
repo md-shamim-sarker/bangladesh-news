246a417cc4458090fcff776d1a015965f662f8a3
shamimspro <shamimspro@gmail.com>
2022-09-03 03:17:46 +0600

    remain some logic

--- js/app.js
+++ js/app.js
@@ -11,13 +11,20 @@ const showCategoriesName = response => {
     const categoryUl = document.getElementById('category-ul');
     response.data.news_category.forEach(element => {
         const categoryLi = document.createElement('li');
+        // categoryLi.setAttribute('onclick', 'categoryClickHandler()');
         categoryLi.innerText = element.category_name;
         categoryUl.appendChild(categoryLi);
     });
 };
 loadCategoriesName();
 
-
+// const categoryClickHandler = () => {
+//     const options = {method: 'GET'};
+//     fetch('https://openapi.programming-hero.com/api/news/categories', options)
+//         .then(response => response.json())
+//         .then(response => console.log(response.data))
+//         .catch(err => console.error(err));
+// };
 
 // Fetching Category by Category ID
 const loadCategoriesById = (category_id) => {
@@ -32,19 +39,61 @@ const showCategoriesById = response => {
     const sortCategoriesArray = response.data.sort((a, b) => {
         return b.total_view - a.total_view;
     });
+    const parentMain = document.getElementById('parent-main');
     sortCategoriesArray.forEach(element => {
-        console.log(element.thumbnail_url);
-        console.log(element.title);
-        console.log(element.details);
-        console.log(element.author.name);
-        console.log(element.author.published_date);
-        console.log(element.author.img);
-        console.log(element.total_view);
-        console.log(element.rating.number);
+        const authorName = element.author.name ? element.author.name : 'Author Not Found';
+        const totalView = element.total_view ? element.total_view : '0';
+        const details = element.details.slice(0, 500) + '...';
+
+        const childDiv = document.createElement('div');
+        childDiv.classList.add('grid', 'p-2');
+        childDiv.innerHTML = `
+            <div>
+                <img src="${element.thumbnail_url}" alt="thumbnail image" class="w-100">
+            </div>
+            <div class="p-2 d-flex flex-column justify-content-between">
+                <div>
+                    <h2>${element.title}</h2>
+                    <p>${details}</p>
+                </div>
+                <div
+                    class="d-flex flex-column flex-md-row flex-lg-row gap-3 gap-md-0 gap-lg-0 justify-content-between align-items-start me-0 me-md-4 me-lg-5">
+
+                    <div class="d-flex gap-2 text-center">
+                        <div>
+                            <img src="${element.author.img}" alt="author_image" class="author-image">
+                        </div>
+                        <div>
+                            <span>${authorName}</span><br>
+                            <span>Date: ${element.author.published_date}</span>
+                        </div>
+                    </div>
+                    <div class="d-flex gap-5">
+                        <div>
+                            <img src="./images/eye.svg" alt="">
+                            ${totalView}
+                        </div>
+                        <div>
+                            <img src="./images/star-fill.svg" alt="">
+                            <img src="./images/star-fill.svg" alt="">
+                            <img src="./images/star-fill.svg" alt="">
+                            <img src="./images/star-fill.svg" alt="">
+                            <img src="./images/star-half.svg" alt="">
+                        </div>
+                        <div>
+                            <button class="border-0 bg-transparent">
+                                <img src="./images/arrow-right.svg" alt="">
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+        parentMain.appendChild(childDiv);
     });
     console.log('The number of categories are: ' + sortCategoriesArray.length);
 };
-// loadCategoriesById('02');
+loadCategoriesById('01');
 
 
 // Load News By Id
