7f88c16b8f27f8995c3a659b81f7bba7daa81a98
shamimspro <shamimspro@gmail.com>
2022-09-02 23:29:35 +0600

    loadNewsById

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Fetching News Categories
+// Fetching News Categories Name
 
 const loadCategoriesName = () => {
     const options = {method: 'GET'};
@@ -14,4 +14,49 @@ const showCategoriesName = response => {
     });
 };
 
-loadCategoriesName();
+// loadCategoriesName();
+
+// Fetching Category by Category ID
+
+const loadCategoriesById = (category_id) => {
+    const options = {method: 'GET'};
+    fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`, options)
+        .then(response => response.json())
+        .then(response => showCategoriesById(response))
+        .catch(err => console.error(err));
+};
+
+const showCategoriesById = response => {
+    const sortCategoriesArray = response.data.sort((a, b) => {
+        return b.total_view - a.total_view;
+    });
+    sortCategoriesArray.forEach(element => {
+        console.log(element.thumbnail_url);
+        console.log(element.title);
+        console.log(element.details);
+        console.log(element.author.name);
+        console.log(element.author.published_date);
+        console.log(element.author.img);
+        console.log(element.total_view);
+        console.log(element.rating.number);
+    });
+    console.log('The number of categories are: ' + sortCategoriesArray.length);
+};
+
+// loadCategoriesById('02');
+
+const loadNewsById = news_id => {
+    const options = {method: 'GET'};
+    fetch(`https://openapi.programming-hero.com/api/news/${news_id}`, options)
+        .then(response => response.json())
+        .then(response => showNewsById(response))
+        .catch(err => console.error(err));
+};
+
+const showNewsById = response => {
+    console.log(response.data[0].image_url);
+    console.log(response.data[0].title);
+    console.log(response.data[0].details);
+};
+
+loadNewsById('2e78e5e0310c2e9adbb6efb1a263e745');
