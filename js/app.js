8bd06be115c6049563486021c4be39737963d11f
shamimspro <shamimspro@gmail.com>
2022-09-03 12:00:48 +0600

    no results found issue solved

--- js/app.js
+++ js/app.js
@@ -7,44 +7,50 @@ const loadCategoriesName = () => {
         .catch(err => console.error(err));
 };
 
+// Show all categories name for navigation
 const showCategoriesName = response => {
     const categoryUl = document.getElementById('category-ul');
     response.data.news_category.forEach(element => {
         const categoryLi = document.createElement('li');
-        // categoryLi.innerHTML = `
-        //     <button onclick="loadCategoriesById('${category.data[0].category_id}')">${element.category_name}</button>
-        // `;
-        categoryLi.innerText = element.category_name;
+        categoryLi.innerHTML = `
+            <button id='category-btn' class='btn btn-primary' onclick="loadCategoriesById('${element.category_id}', '${element.category_name}')">${element.category_name}</button>
+        `;
         categoryUl.appendChild(categoryLi);
-        // categoryLi.setAttribute('onclick', `${loadCategoriesById(response.data[0].category_id)}`);
     });
 };
 loadCategoriesName();
 
-
-
 // Fetching Category by Category ID
-const loadCategoriesById = category_id => {
+const loadCategoriesById = (category_id, category_name) => {
     const options = {method: 'GET'};
     fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`, options)
         .then(response => response.json())
-        .then(response => showCategoriesById(response))
+        .then(response => showCategoriesById(response, category_name))
         .catch(err => console.error(err));
 };
 
-const showCategoriesById = category => {
+// Show All Categoris by Id
+const showCategoriesById = (category, category_name) => {
+
+    // Sort Operation Start
     const sortCategoriesArray = category.data.sort((a, b) => {
         return b.total_view - a.total_view;
     });
-    const parentMain = document.getElementById('parent-main');
-    sortCategoriesArray.forEach(element => {
-        const authorName = element.author.name ? element.author.name : 'Author Not Found';
-        const totalView = element.total_view ? element.total_view : '0';
-        const details = element.details.slice(0, 500) + '...';
+    // Sort Operation End
 
-        const childDiv = document.createElement('div');
-        childDiv.classList.add('grid', 'p-2');
-        childDiv.innerHTML = `
+    let parentMain = document.getElementById('parent-main');
+    parentMain.innerHTML = '';
+
+    const arrLength = sortCategoriesArray.length;
+
+    if(arrLength > 0) {
+        sortCategoriesArray.forEach(element => {
+            const authorName = element.author.name ? element.author.name : 'Author Not Found';
+            const totalView = element.total_view ? element.total_view : '0';
+            const details = element.details.slice(0, 500) + '...';
+            const childDiv = document.createElement('div');
+            childDiv.classList.add('grid', 'p-2');
+            childDiv.innerHTML = `
             <div>
                 <img src="${element.thumbnail_url}" alt="thumbnail image" class="w-100">
             </div>
@@ -55,7 +61,6 @@ const showCategoriesById = category => {
                 </div>
                 <div
                     class="d-flex flex-column flex-md-row flex-lg-row gap-3 gap-md-0 gap-lg-0 justify-content-between align-items-start me-0 me-md-4 me-lg-5">
-
                     <div class="d-flex gap-2 text-center">
                         <div>
                             <img src="${element.author.img}" alt="author_image" class="author-image">
@@ -86,12 +91,18 @@ const showCategoriesById = category => {
                 </div>
             </div>
         `;
-        parentMain.appendChild(childDiv);
-    });
-    console.log('The number of categories are: ' + sortCategoriesArray.length);
-    console.log(category.data[0].category_id);
+            parentMain.appendChild(childDiv);
+        });
+    } else {
+        parentMain.innerHTML = `
+            <h2 class='text-center text-warning'>No News Found.</h2>
+        `;
+    }
+    document.getElementById('item-numbers').innerText = sortCategoriesArray.length;
+    document.getElementById('category-name').innerText = category_name;
 };
-loadCategoriesById('02');
+// Show News (Default)
+loadCategoriesById('02', 'Regular News');
 
 
 // Load News By Id
